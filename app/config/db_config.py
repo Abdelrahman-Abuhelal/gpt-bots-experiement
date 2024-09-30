e1e931b67dd997be10906aafd1cc3c8b13b357db
Abdelrahman-Abuhelal <abd.hilal14@gmail.com>
2024-09-30 12:40:41 +0200

    database configs

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,20 +1,18 @@
 # config/db_config.py
-from flask_sqlalchemy import SQLAlchemy
 from langchain_community.utilities.sql_database import SQLDatabase
 
-class DBConfig:
-    def get_sqlalchemy_config(self, app):
-        """
-        Configure and initialize SQLAlchemy for the Flask app.
-        """
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///chat.db"
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        sqlalchemy_db = SQLAlchemy(app)
-        return sqlalchemy_db
 
-    def get_mysql_db(self):
+class DBConfig:
+    def configure_sqlalchemy(self, app):
         """
-        Configure and initialize MySQL connection using SQLDatabase.
+        Configure SQLAlchemy for the Flask app.
+        """
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    def configure_mysql(self):
+        """
+        Configure MySQL connection using SQLDatabase.
         """
         db_user = "root"
         db_password = "ROOT1234"
@@ -22,10 +20,6 @@ class DBConfig:
         db_name = "training_management"
 
         # Create a MySQL database connection instance
-        mysql_db = SQLDatabase.from_uri(
+        return SQLDatabase.from_uri(
             f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
         )
-        # print(mysql_db.dialect)
-        # print(mysql_db.get_usable_table_names())
-        # print(mysql_db.table_info)
-        return mysql_db
---
+++ app/models/chat_message.py
@@ -0,0 +1,7 @@
+from ..extensions import db  # Import db from extensions
+
+
+class ChatMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=False)
